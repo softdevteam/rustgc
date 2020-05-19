9cf123d2653d34a809ce0dafe2da0a9abe4614eb
Jacob Hughes <jh@jakehughes.uk>
2020-05-19 20:10:08 +0100

    Add API for Gc smart pointer
    The Gc type is re-exported so that it can be used in user code similarly
    to other smart pointer types (e.g. Rc) as follows:
    
    	use std::gc::Gc;

--- src/libstd/gc.rs
+++ src/libstd/gc.rs
@@ -6,6 +6,9 @@ use crate::alloc_crate::boehm::GC_gcollect;
 #[unstable(feature = "gc", issue = "none")]
 pub use core::gc::*;
 
+#[unstable(feature = "gc", issue = "none")]
+pub use alloc_crate::gc::*;
+
 #[unstable(feature = "gc", reason = "gc", issue="none")]
 pub fn force_collect() {
     unsafe { GC_gcollect() };
