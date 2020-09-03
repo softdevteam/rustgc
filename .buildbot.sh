90fab6bf560e4aa06ac9a542384ce8bcae68c9a0
Jacob Hughes <jh@jakehughes.uk>
2020-09-03 09:25:44 +0100

    Fix buildbot script
    After the change from src/libstd to library/ we can omit the old
    directory name for the test command.
    
    The tidy checker is also disabled until I can work out how to exclude
    boehm sources.

--- .buildbot.sh
+++ .buildbot.sh
@@ -7,4 +7,4 @@ set -e
 # Ensure the build fails if it uses excessive amounts of memory.
 ulimit -d $((1024 * 1024 * 8)) # 8 GiB
 
-./x.py test src/libstd --stage 1
+./x.py test --exclude src/tools/tidy --stage 1
