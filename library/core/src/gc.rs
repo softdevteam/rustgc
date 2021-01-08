9fe3da9a9287e5776d8cf543d9afc826b5466fcf
Jacob Hughes <jh@jakehughes.uk>
2021-01-08 15:53:10 +0000

    Remove unused ManageableContents trait in libcore

--- library/core/src/gc.rs
+++ library/core/src/gc.rs
@@ -6,13 +6,6 @@ static MAX_LAYOUT: usize = crate::mem::size_of::<usize>() * 64;
 
 
 #[unstable(feature = "gc", issue = "none")]
-#[cfg_attr(not(bootstrap), lang = "manageable_contents")]
-/// This trait can be implemented on types where it is safe to allow the allow the collector to
-/// free its memory and omit the drop method. This prevents the need to register a finalizer when
-/// managed by the Gc which is expensive.
-pub trait ManageableContents {}
-
-#[unstable(feature = "gc", issue = "none")]
 #[cfg_attr(not(bootstrap), lang = "no_finalize")]
 pub trait NoFinalize {}
 
